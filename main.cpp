31d642a9c0ee81b77f683c307fdc7413e2da2c0f
masataka <ar.tec.masa@gmail.com>
2019-04-21 21:24:27 +0900

    add (std-list)

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <list>
 
 int main()
 {
+    // 目次
     // 1. リスト
     // 2. 配列
     // 3. スタック
@@ -9,4 +11,19 @@ int main()
     // 5. ハッシュテーブル
     // 6. ヒープ
     // 7. 2分探索木
+
+    //// リスト ////
+    // データを一直線に並べた構造を持っている。データの追加や削除がしやすい反面，アクセスに時間がかかる。
+
+    std::list<int> lst;
+
+    class NumList
+    {
+    public:
+    private:
+    public:
+        NumList(){}
+        ~NumList(){}
+    private:
+    };
 }
